14a52485b7842718d46dfd93d7576b9c8e83719b
unknown <chobonjiri@gmail.com>
2025-06-02 04:09:28 +0900

    rererererereretry

---
+++ ui/damageCalc.js
@@ -0,0 +1,9 @@
+// damageCalc.js
+export function calculateDamage(attacker, defender, move, conditions) {
+  // 計算ロジック
+  return {
+    minDamage: 50,
+    maxDamage: 70,
+    criticalDamage: 90,
+  };
+}
