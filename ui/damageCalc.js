da4fa575a9d0f45091b3d7a4dae4b2f220b2afe1
mini-bonjiri <49784487+perzexo@users.noreply.github.com>
2025-06-02 16:12:31 +0000

    sakujyo

--- ui/damageCalc.js
+++
@@ -1,9 +0,0 @@
-// damageCalc.js
-export function calculateDamage(attacker, defender, move, conditions) {
-  // 計算ロジック
-  return {
-    minDamage: 50,
-    maxDamage: 70,
-    criticalDamage: 90,
-  };
-}
